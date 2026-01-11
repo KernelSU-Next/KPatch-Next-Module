f6cef0b8391d5263421388d16f947b7bb8057200
KOWX712 <leecc0503@gmail.com>
2026-01-11 10:15:52 +0800

    scripts/post-fs-data: always overwrite status.sh

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -4,8 +4,6 @@ MODDIR=${0%/*}
 SERVICE_D="/data/adb/service.d"
 STATUS_SH="$SERVICE_D/kp-next.sh"
 
-if [ ! -d "$SERVICE_D" ] || [ ! -f "$STATUS_SH" ]; then
-    mkdir -p "$SERVICE_D"
-    cp "$MODDIR/status.sh" "$STATUS_SH"
-    chmod 755 "$STATUS_SH"
-fi
+mkdir -p "$SERVICE_D"
+cp "$MODDIR/status.sh" "$STATUS_SH"
+chmod 755 "$STATUS_SH"
